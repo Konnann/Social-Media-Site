d2a1e83a21e7cb80bc8ad417c29926b44bd7c94d
failfmi <fifty_cen7@abv.bg>
2016-08-10 11:25:44 +0300

    SoftwareTechnologiesProject
    ATHNN

---
+++ ATHNN/ATHNN/Models/Comment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ATHNN.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Text { get; set; }
+
+        [Required]
+        public int PostId { get; set; }
+
+        [Required]
+        public int AuthorId { get; set; }
+
+        [Required]
+        public string AuthorName { get; set; }
+
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        public ApplicationUser Author { get; set; }
+        public Post Post { get; set; }
+
+    }
+}
---
+++ ATHNN/ATHNN/Models/Post.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ATHNN.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        public ApplicationUser Author { get; set; }
+    }
+}
