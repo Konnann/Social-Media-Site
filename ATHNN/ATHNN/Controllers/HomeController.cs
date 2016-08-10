73abb1efe2b746e82ec6edc148054b37ba1acb77
gdavramov <r1ox@hotmail.com>
2016-08-10 11:20:39 +0300

    SoftwareTechnologies Project

---
+++ ATHNN/ATHNN/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ATHNN.Models;
+
+namespace ATHNN.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            var posts = db.Posts.Include(p => p.Author).OrderByDescending(p => p.Date).Take(5);
+            ViewBag.SideBarPosts = posts;
+            return View(posts.Take(3).ToList());
+        }
+
+    }
+}
