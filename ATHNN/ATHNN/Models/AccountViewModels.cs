130b0d0c3ed9eeaaa2b462859c613b64a72a69d0
failfmi <fifty_cen7@abv.bg>
2016-08-23 18:15:23 +0300

    updated log stuff
    updated log stuff

--- ATHNN/ATHNN/Models/AccountViewModels.cs
+++ ATHNN/ATHNN/Models/AccountViewModels.cs
@@ -49,9 +49,9 @@ namespace ATHNN.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "Username")]
+        [StringLength(256)]
+        public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
@@ -68,6 +68,11 @@ namespace ATHNN.Models
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name="Username")]
+        public string UserName { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
