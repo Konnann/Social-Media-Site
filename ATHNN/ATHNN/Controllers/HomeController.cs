df2f3247d53056aa7bd3ea834b8b128853ed3f7c
Kamen Petrov <kamen.petrov.official@gmail.com>
2016-08-26 17:42:51 +0300

    Added Image support

--- ATHNN/ATHNN/Controllers/HomeController.cs
+++ ATHNN/ATHNN/Controllers/HomeController.cs
@@ -10,12 +10,116 @@ namespace ATHNN.Controllers
 {
     public class HomeController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+        private ApplicationDbContext db = new ApplicationDbContext ( );
+
+        public static bool IsValidURI ( string uri )
+        {
+            if ( !Uri.IsWellFormedUriString (uri, UriKind.Absolute) )
+                return false;
+            Uri tmp;
+            if ( !Uri.TryCreate (uri, UriKind.Absolute, out tmp) )
+                return false;
+            return tmp.Scheme == Uri.UriSchemeHttp || tmp.Scheme == Uri.UriSchemeHttps;
+        }
+
+        public ActionResult Gaming ( )
+        {
+            // Listing tagged posts 
+
+            string[] tagsForListing = new string[] { "game", "gaming" };
+
+
+            //List<Post> taggedPosts = new List<Post>();
+            //var posts = db.Posts;
+            ////var p = db.Posts.Where(x => x.Id == 2).Include(x => x.Tags);
+
+            //Tag tag = db.Tags.Single(t => t.TagId == 4);
+            //var tagPosts = db.Posts.ToList();
+            //tagPosts = tagPosts.Where(p => p.Tags.Contains(tag)).ToList();
+
+            IQueryable<Post> postByTag = db.Posts;
+            List<Post> posts = new List<Post> ( );
+            foreach ( var tagText in tagsForListing )
+            {
+                posts.AddRange (postByTag.Where (p => p.Tags.Select (t => t.Name).Contains (tagText)).ToList ( ));
+            }
+
+            ViewBag.TaggedPosts = posts.Distinct ( );
+
+            //Check if image is existing
+            List<bool> postsExist = new List<bool> ( );
+            foreach ( var post in posts )
+            {
+                if ( IsValidURI (post.Body) )
+                {
+                    postsExist.Add (true);
+                }
+                else
+                {
+                    postsExist.Add (false);
+                }
+
+                /*
+                    HttpWebResponse response = null;
+                    var request = ( HttpWebRequest ) WebRequest.Create (post.Body);
+                    request.Method = "HEAD";
+
+
+                    try
+                    {
+                        response = ( HttpWebResponse ) request.GetResponse ( );
+                        postsExist.Add (true);
+                    }
+                    catch ( WebException ex )
+                    {
+
+
+                    postsExist.Add (false);
+                }
+                    finally
+                {
+                    // Don't forget to close your response.
+                    if ( response != null )
+                    {
+                        response.Close ( );
+                    }
+
+                }
+                */
+
+            }
+
+
+            ViewBag.PostExists = postsExist;
+            ViewBag.TaggedPosts = posts.Distinct ( );
+
+            return View ( );
+        }
+
+
         public ActionResult Index()
         {
-            var posts = db.Posts.Include(p => p.Author).OrderByDescending(p => p.Date).Take(5);
+
+
+
+            var posts = db.Posts.Include(p => p.Author).OrderByDescending(p => p.Date);
+
+            List<bool> postsExist = new List<bool> ( );
+            foreach (var post in posts)
+            {
+                if (IsValidURI(post.Body))
+                {
+                    postsExist.Add(true);
+                }
+                else
+                {
+                    postsExist.Add(false);
+                }
+            }
+
             ViewBag.SideBarPosts = posts;
-            return View(posts.Take(3).ToList());
+            ViewBag.PostExists = postsExist;
+            return View(posts.ToList());
         }
 
     }
--- ATHNN/ATHNN/Controllers/PostsController.cs
+++ ATHNN/ATHNN/Controllers/PostsController.cs
@@ -11,146 +11,123 @@ using ATHNN.Models;
 
 namespace ATHNN.Controllers
 {
-    [ValidateInput(false)]
+    [ValidateInput (false)]
     public class PostsController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext ( );
 
-        public ActionResult Gaming ( )
-        {
-            string[] tagsForListing = new string[] { "game","gaming"};
-
-
-            //List<Post> taggedPosts = new List<Post>();
-            //var posts = db.Posts;
-            ////var p = db.Posts.Where(x => x.Id == 2).Include(x => x.Tags);
-            
-            //Tag tag = db.Tags.Single(t => t.TagId == 4);
-            //var tagPosts = db.Posts.ToList();
-            //tagPosts = tagPosts.Where(p => p.Tags.Contains(tag)).ToList();
-            IQueryable<Post> postByTag =db.Posts;
-            List<Post> posts = new List<Post>();
-            foreach (var tagText in tagsForListing)
-            {
-                posts.AddRange(postByTag.Where(p => p.Tags.Select(t => t.Name).Contains(tagText)).ToList());
-            }
-            
-            ViewBag.TaggedPosts = posts.Distinct();
-            return View ();
-        }
-
         // GET: Posts
         public ActionResult Index ( )
+    {
+        var postsWithAuthors = db.Posts.Include (p => p.Author).ToList ( );
+        return View (postsWithAuthors);
+    }
+
+    // GET: Posts/Details/5
+    public ActionResult Details ( int? id )
+    {
+        if ( id == null )
         {
-            var postsWithAuthors = db.Posts.Include(p => p.Author).ToList();
-            return View (postsWithAuthors);
+            return new HttpStatusCodeResult (HttpStatusCode.BadRequest);
         }
-
-        // GET: Posts/Details/5
-        public ActionResult Details ( int? id )
+        Post post = db.Posts.Find (id);
+        if ( post == null )
         {
-            if ( id == null )
-            {
-                return new HttpStatusCodeResult (HttpStatusCode.BadRequest);
-            }
-            Post post = db.Posts.Find (id);
-            if ( post == null )
-            {
-                return HttpNotFound ( );
-            }
-            return View (post);
+            return HttpNotFound ( );
         }
+        return View (post);
+    }
 
-        // GET: Posts/Create
-        public ActionResult Create ( )
+    // GET: Posts/Create
+    public ActionResult Create ( )
+    {
+        return View ( );
+    }
+
+    // POST: Posts/Create
+    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create ( [Bind (Include = "Id,Title,Body,Tags")] Post post )
+    {
+        if ( ModelState.IsValid )
         {
-            return View ( );
-        }
-
-        // POST: Posts/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create ( [Bind (Include = "Id,Title,Body,Tags")] Post post )
-        {
-            if ( ModelState.IsValid )
-            {
-                post.Author = db.Users.FirstOrDefault(user => user.UserName == User.Identity.Name);
-                post.Date = DateTime.Now;
-                db.Posts.Add (post);
-                db.SaveChanges ();
-                return RedirectToAction ("Index");
-            }
-
-            return View (post);
-        }
-
-        // GET: Posts/Edit/5
-        public ActionResult Edit ( int? id )
-        {
-            if ( id == null )
-            {
-                return new HttpStatusCodeResult (HttpStatusCode.BadRequest);
-            }
-            Post post = db.Posts.Find (id);
-            if ( post == null )
-            {
-                return HttpNotFound ( );
-            }
-            return View (post);
-        }
-
-        // POST: Posts/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit ( [Bind (Include = "Id,Title,Body,Tags")] Post post )
-        {
-            if ( ModelState.IsValid )
-            {
-                post.Date=DateTime.Now;
-                db.Entry (post).State = EntityState.Modified;
-                db.SaveChanges ( );
-                return RedirectToAction ("Index");
-            }
-            return View (post);
-        }
-
-        // GET: Posts/Delete/5
-        public ActionResult Delete ( int? id )
-        {
-            if ( id == null )
-            {
-                return new HttpStatusCodeResult (HttpStatusCode.BadRequest);
-            }
-            Post post = db.Posts.Find (id);
-            if ( post == null )
-            {
-                return HttpNotFound ( );
-            }
-            return View (post);
-        }
-
-        // POST: Posts/Delete/5
-        [HttpPost, ActionName ("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed ( int id )
-        {
-            Post post = db.Posts.Find (id);
-            db.Posts.Remove (post);
+            post.Author = db.Users.FirstOrDefault (user => user.UserName == User.Identity.Name);
+            post.Date = DateTime.Now;
+            db.Posts.Add (post);
             db.SaveChanges ( );
             return RedirectToAction ("Index");
         }
 
-        protected override void Dispose ( bool disposing )
-        {
-            if ( disposing )
-            {
-                db.Dispose ( );
-            }
-            base.Dispose (disposing);
-        }
+        return View (post);
     }
+
+    // GET: Posts/Edit/5
+    public ActionResult Edit ( int? id )
+    {
+        if ( id == null )
+        {
+            return new HttpStatusCodeResult (HttpStatusCode.BadRequest);
+        }
+        Post post = db.Posts.Find (id);
+        if ( post == null )
+        {
+            return HttpNotFound ( );
+        }
+        return View (post);
+    }
+
+    // POST: Posts/Edit/5
+    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit ( [Bind (Include = "Id,Title,Body,Tags")] Post post )
+    {
+        if ( ModelState.IsValid )
+        {
+            post.Date = DateTime.Now;
+            db.Entry (post).State = EntityState.Modified;
+            db.SaveChanges ( );
+            return RedirectToAction ("Index");
+        }
+        return View (post);
+    }
+
+    // GET: Posts/Delete/5
+    public ActionResult Delete ( int? id )
+    {
+        if ( id == null )
+        {
+            return new HttpStatusCodeResult (HttpStatusCode.BadRequest);
+        }
+        Post post = db.Posts.Find (id);
+        if ( post == null )
+        {
+            return HttpNotFound ( );
+        }
+        return View (post);
+    }
+
+    // POST: Posts/Delete/5
+    [HttpPost, ActionName ("Delete")]
+    [ValidateAntiForgeryToken]
+    public ActionResult DeleteConfirmed ( int id )
+    {
+        Post post = db.Posts.Find (id);
+        db.Posts.Remove (post);
+        db.SaveChanges ( );
+        return RedirectToAction ("Index");
+    }
+
+    protected override void Dispose ( bool disposing )
+    {
+        if ( disposing )
+        {
+            db.Dispose ( );
+        }
+        base.Dispose (disposing);
+    }
+}
 }
