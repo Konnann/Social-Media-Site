d194ecdde77d167d40bfd58bfb7ce2addb7273a3
Konan <j.vasileva@abv.bg>
2016-08-30 09:51:52 +0300

    Second try at uploading the profile page. Displays user's posts, needs more work

---
+++ ATHNN/ATHNN/Controllers/ProfileController.cs
@@ -0,0 +1,40 @@
+﻿using ATHNN.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+
+namespace ATHNN.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Profile
+        public ActionResult MyPosts()
+        {
+            //Select all posts from logged in user
+            var userPosts = db.Posts.Where(p => p.Author.UserName == User.Identity.Name).Include(p => p.Author).OrderByDescending(p => p.Date).ToList();
+
+            List<bool> postsExist = new List<bool>();
+            foreach (var post in userPosts)
+            {
+                if (HomeController.IsValidURI(post.Body))
+                {
+                    postsExist.Add(true);
+                }
+                else
+                {
+                    postsExist.Add(false);
+                }
+            }
+            ViewBag.PostExists = postsExist;
+            TempData["PostExists"] = postsExist;
+
+            return View(userPosts);
+        }
+    }
+}
